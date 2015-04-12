b6604b5f986ecc420f258d025bbc9f1389261442
Maxim Syrykh <slogger1994@gmail.com>
2015-04-12 18:43:40 +0500

    Fix lost server

--- sntp/sntp-server.py
+++ sntp/sntp-server.py
@@ -1,89 +1,70 @@
 #!/usr/bin/python3
 
 import argparse
-from select import select
-from time import time, strftime, gmtime
-from socket import socket, AF_INET, SOCK_DGRAM
+from time import time
+from random import randint
+import socket
+from struct import pack, unpack
+import asyncio
 from common import *
-from hexdump import *
 
 
-def get_args():
-    parser = argparse.ArgumentParser(
-        description="NTP tool")
+class NTPServer(asyncio.Protocol):
+    def connection_made(self, transport):
+        self.transport = transport
+
+    def datagram_received(self, data, addr):
+        packet = packet_from_binary(data)
+        packet.receive_binary = utc_to_ntp_bytes(time())
+        packet.stratum = 2
+        packet.options = 36
+        packet.precision_binary = randint(0, 256)
+        packet.ref_id_binary = get_my_ip()
+        packet.origin_binary = utc_to_ntp_bytes(time() + (args.delay * 2))
+        packet.ref_time_binary = utc_to_ntp_bytes(time())
+        r = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        r.connect(addr)
+        packet = packet.to_binary()
+        self.transport.sendto(packet, addr)
+
+
+def get_args_parser():
+    parser = argparse.ArgumentParser(description="NTP Server")
     parser.add_argument(
-        "source",
-        help="Source server address")
+        "--delay",
+        help="Set delay",
+        type=int,
+        default=0)
     parser.add_argument(
-        "-v", "--version",
-        help="NTP version to be used",
-        default=NTP_CURRENT_VERSION,
-        type=int)
-    parser.add_argument(
-        "-t", "--timeout",
-        help="Timeout in seconds (default 1)",
-        default=1,
-        type=int)
-    parser.add_argument(
-        "-a", "--attempts",
-        help="Maximum attempts (default 5)",
-        default=5,
-        type=int)
-    parser.add_argument(
-        "-f", "--file",
-        help="Use source as filename of NTP packet dump",
-        action='store_true')
-    parser.add_argument(
-        "-u", "--show-utc",
-        help="Show UTC interpretation of time",
-        action='store_true')
+        "-p", "--port",
+        help="port",
+        type=int,
+        default=5000)
+    return parser
+
+
+def get_my_ip():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('urfu.ru', 0))
+        ip = s.getsockname()[0]
+        return bytes([int(x) for x in ip.split('.')])
+    except socket.gaierror:
+        return bytes([0, 0, 0, 0])
+
+
+if __name__ == '__main__':
+    parser = get_args_parser()
     args = parser.parse_args()
-    return args
+    loop = asyncio.get_event_loop()
+    listen = loop.create_datagram_endpoint(
+        NTPServer, local_addr=('127.0.0.1', args.port))
+    transport, protocol = loop.run_until_complete(listen)
 
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
 
-def get_address(source):
-    """Return address and port"""
-    chunks = source.split(':')
-    return chunks[0], int(chunks[1]) if len(chunks) > 1 else NTP_PORT
-
-
-def get_raw_packet(args):
-    """Get packet"""
-    if args.file:
-        try:
-            with open(args.source, "rb") as file:
-                return file.read()
-        except OSError as e:
-            exit(e)
-    request = request_packet(version=args.version).to_binary()
-    for attempt in range(1, args.attempts + 1):
-        try:
-            address = get_address(args.source)
-            with socket(AF_INET, SOCK_DGRAM) as sock:
-                sock.sendto(request, address)
-                if select([sock], [], [], args.timeout)[0]:
-                    return sock.recvfrom(1024)[0]
-        except Exception as e:
-            pass
-        print("Attempt {} failed".format(attempt))
-    return bytes(0)
-
-
-def get_clock_offset(packet):
-    """Return clock offset"""
-    current_ntp_time = time() + NTP_UTC_OFFSET
-    trip_delay = (packet.receive - packet.origin) / 2
-    return Decimal(current_ntp_time) - packet.transmit - trip_delay
-
-
-if __name__ == "__main__":
-    args = get_args()
-    raw_packet = get_raw_packet(args)
-    if raw_packet:
-        packet = packet_from_binary(raw_packet)
-        print(get_packet_hexdump(packet, args.show_utc))
-        clock_offset = get_clock_offset(packet)
-        print('Local clock offset: {} sec'.format(
-            clock_offset))
-    else:
-        print("Failed")
+    transport.close()
+    loop.close()
